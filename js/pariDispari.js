da922899925955306bdb82350c8c1cb80382b867
Amarildo <amarildos@outloo.it>
2024-01-09 16:11:22 +0100

    get element by html file

--- js/pariDispari.js
+++ js/pariDispari.js
@@ -1,5 +1,13 @@
+console.log('JS OK')
 // 1. Chiediamo all'utente di scegliere tra pari o dispari e inserire un numero da 1 a 5.
 // 2. Generiamo un numero casuale per il computer.
 // 3. Sommiamo i due numeri.
 // 4. Stabiliamo se la somma è pari o dispari.
 // 5. Diciamo chi ha vinto.
+
+//recupero elementi
+const sceltaElement = document.getElementById('scelta');
+const numeroElement = document.getElementById('numero');
+const risultatoElement = document.getElementById('risultato');
+
+// 2. Generiamo un numero casuale per il computer.
