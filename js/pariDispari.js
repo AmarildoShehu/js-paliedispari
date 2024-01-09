0d2a6a295362ebc58b321ae17ede232444faed29
Amarildo <amarildos@outloo.it>
2024-01-09 15:59:19 +0100

    added in pariDispari my ladder

---
+++ js/pariDispari.js
@@ -0,0 +1,5 @@
+// 1. Chiediamo all'utente di scegliere tra pari o dispari e inserire un numero da 1 a 5.
+// 2. Generiamo un numero casuale per il computer.
+// 3. Sommiamo i due numeri.
+// 4. Stabiliamo se la somma è pari o dispari.
+// 5. Diciamo chi ha vinto.
