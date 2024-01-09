4e9bcc9b03389f27a43127a3ea9beef22d545fa7
Amarildo <amarildos@outloo.it>
2024-01-09 15:29:42 +0100

    aded function and print

---
+++ js/pills.js
@@ -0,0 +1,14 @@
+/*
+function printHello(){
+console.log('hello World!');
+}
+printHello();*/
+
+// Esercizio 2A
+function getOneStringFromTwo(firstString, secondString) {
+    return firstString + " " + secondString;
+    
+}
+  // Esempio di utilizzo della funzione
+   console.log(getOneStringFromTwo('hello','world'))
+  
--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ console.log('JS OK');
 // 2. Chiedere all’utente di inserire una parola
 // 3. Creare una funzione per capire se la parola inserita è palindroma
 // 4. Stampiamo il risultato in console
+
+// 2. Chiedere all’utente di inserire una parola
+const parolaUtente = prompt('Inserisci una parola:', 'Amarildo');
+console.log(parolaUtente);
+
+// 3. Creare una funzione per capire se la parola inserita è palindroma
+
+function verificaPalindromo(parola) {
+    const lunghezza = parola.length;
+    for (let i = 0; i < lunghezza / 2; i++) {
+        if (parola[i] !== parola[lunghezza - 1 - i]) return false;
+    }
+    return true;
+}
+
+//VERIFICA
+const risultato = verificaPalindromo(parolaUtente);
+console.log(risultato);
