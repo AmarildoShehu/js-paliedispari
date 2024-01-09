c7d2829c5e42884154d7c9e13705bcc8f92eb668
Amarildo <amarildos@outloo.it>
2024-01-09 14:43:57 +0100

    added my ladder 'Polidroma'

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// 1. collego html e js
+console.log('JS OK');
+// ### Palidroma ####
+// 2. Chiedere all’utente di inserire una parola
+// 3. Creare una funzione per capire se la parola inserita è palindroma
+// 4. Stampiamo il risultato in console
